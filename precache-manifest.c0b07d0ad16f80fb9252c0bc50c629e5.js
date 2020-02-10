545ed48fca5d3e694e45f439941ce016130e9e2d
bannymomo <bannymomo33@gmail.com>
2020-02-10 21:05:33 +1000

    Updates

---
+++ precache-manifest.c0b07d0ad16f80fb9252c0bc50c629e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b79d050fdfdaf52c6308c71e62d8601",
+    "url": "./index.html"
+  },
+  {
+    "revision": "724ae907cebf7a3befdf",
+    "url": "./static/css/main.0947a778.chunk.css"
+  },
+  {
+    "revision": "20b8461817a99010ca68",
+    "url": "./static/js/2.86e97719.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.86e97719.chunk.js.LICENSE"
+  },
+  {
+    "revision": "724ae907cebf7a3befdf",
+    "url": "./static/js/main.9c3c6545.chunk.js"
+  },
+  {
+    "revision": "7aee5c31a32a1a88dbc2",
+    "url": "./static/js/runtime-main.75186b59.js"
+  }
+]);
